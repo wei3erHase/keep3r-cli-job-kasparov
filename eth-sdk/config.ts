3cc7d59810354e809deae3cb60a6bc6357aa0b72
wei3erHase <wei3erhase@defi.sucks>
2022-07-23 21:56:42 +0200

    feat: try to workMany

--- eth-sdk/config.ts
+++ eth-sdk/config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
   contracts: {
     mainnet: {
       kasparov: '0x54A8265ADC50fD66FD0F961cfCc8B62DE0f2B57f',
+      keep3r: '0xeb02addCfD8B773A5FFA6B9d1FE99c566f8c44CC',
+      keep3rHelper: '0xd36ac9ff5562abb541f51345f340fb650547a661',
     },
   },
 });
